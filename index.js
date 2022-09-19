75d3321898d57b7c3e6516d9560c79831aa790c1
Krishna Gite <>
2022-09-19 11:41:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { log } = require("console");
+var rlsync = require("readline-sync");
+var readuser=rlsync.question("Hello, type your name ");
+
+highestScorer={
+  name:"Krishna",
+  score:0
+}
+
+console.log("\nThe highest scorer of this game upto now is: ",highestScorer.name,"with a score: ",highestScorer.score)
+console.log("Hii", readuser,"Do you know Krishna? Lets see:");
+
+var questionbank =[{
+  question:"Where does he live? ",
+  answer:"Mumbai"
+},{
+  question:"Where would he go for a trip? ",
+  answer:"Goa"
+},{
+  question:"Whats his age? ",
+  answer:"21"
+},{
+  question:"What sport does he play? ",
+  answer:"Football"
+},{
+  question:"What art does he follow? ",
+  answer:"Music"
+}]
+
+var score=0
+
+for(let i=0;i<questionbank.length;i++){
+  var useranswer=rlsync.question(questionbank[i].question);
+  if(useranswer.toLowerCase()===questionbank[i].answer.toLowerCase()){
+    score++;
+    console.log("Right!\n");
+  }else{
+    console.log("You're wrong.\n");
+  }
+  console.log("Your current score is:", score);
+}
+console.log(readuser,",your final score is:",score);
+
+if(score>highestScorer.score){
+  highestScorer.name=readuser;
+  highestScorer.score=score;
+  console.log("New highest scorer: ",highestScorer.name);
+}
